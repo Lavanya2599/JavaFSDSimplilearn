1fd6cd5726084699b7ceccc93cfff784eed3246c
Lavanya S <83653969+Lavanya2599@users.noreply.github.com>
2023-05-24 14:52:13 +0530

    Add files via upload

---
+++ ValidationUserLogin/src/main/java/DashboardServlet.java
@@ -0,0 +1,50 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DashboardServlet
+ */
+@WebServlet("/DashboardServlet")
+public class DashboardServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DashboardServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+
+        // Display the dashboard page
+        response.getWriter().println("<h1>Welcome to Dashboard</h1>");
+        response.getWriter().println("<a href='logout'>Logout</a>");
+        response.getWriter().println("<title>Dashboard</title>");
+        response.getWriter().println("<style>\r\n"
+        		+ "        body {\r\n"
+        		+ "            background-color: #00FF00;\r\n"
+        		+ "        }\r\n"
+        		+ "    </style>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ ValidationUserLogin/src/main/java/ErrorServlet.java
@@ -0,0 +1,49 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ErrorServlet
+ */
+@WebServlet("/ErrorServlet")
+public class ErrorServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ErrorServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+
+        // Display the error message
+        response.getWriter().println("<h1>Error: Incorrect login credentials</h1>");
+        response.getWriter().println("<a href='login.html'>Go back to Login</a>");
+        response.getWriter().println("<style>\r\n"
+        		+ "        body {\r\n"
+        		+ "            background-color: #FF0000;\r\n"
+        		+ "        }\r\n"
+        		+ "    </style>");
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
